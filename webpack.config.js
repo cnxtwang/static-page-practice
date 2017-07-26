355b836fd4dabd39bf476577a29acd6b5ca39673
xtwang <xtwang@thoughtworks.com>
2017-07-26 09:51:04 +0800

    initial project

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './src/js/invitation.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist/',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(scss|sass)$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png)$/,
+        loader: 'url-loader'
+      }
+    ]
+  }
+}
